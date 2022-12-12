4ee268643e0a6829da9a0303e3309475be6070a7
Geo:) <geovanna.medina@outlook.com>
2022-12-12 00:06:11 -0600

    hf: view dummy example changes

--- src/main/java/com/beluga/impl/urnaVirtual/UrnaVirtualView.java
+++ src/main/java/com/beluga/impl/urnaVirtual/UrnaVirtualView.java
@@ -2,7 +2,6 @@ package com.beluga.impl.urnaVirtual;
 
 import java.awt.Dimension;
 
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,11 +10,13 @@ import com.beluga.framework.mvc.Controller;
 import com.beluga.framework.mvc.Model;
 import com.beluga.framework.mvc.View;
 
-
 public class UrnaVirtualView extends View {
 
     private JFrame frame;
-    JLabel label;
+
+    JLabel label1;
+    JLabel label2;
+    JLabel label3;
     int contador = 0;
 
     public UrnaVirtualView(Model model, Controller controller) {
@@ -30,7 +31,7 @@ public class UrnaVirtualView extends View {
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setLayout(null);
         // set the jframe size and location
-        this.frame.setPreferredSize(new Dimension(600, 150));
+        this.frame.setPreferredSize(new Dimension(600, 200));
         this.frame.pack();
         this.frame.setLocationRelativeTo(null);
 
@@ -47,17 +48,25 @@ public class UrnaVirtualView extends View {
         btnCnddt2.addActionListener(this.controller);
         btnCnddt3.addActionListener(this.controller);
 
-
-
         this.frame.add(btnCnddt1);
         this.frame.add(btnCnddt2);
         this.frame.add(btnCnddt3);
 
-        label = new JLabel();
+        label1 = new JLabel();
+        label2 = new JLabel();
+        label3 = new JLabel();
 
-        label.setBounds(250, 70, 35, 35);
+        label1.setText("0");
+        label2.setText("0");
+        label3.setText("0");
 
-        this.frame.add(label);
+        label1.setBounds(100, 90, 100, 35);
+        label2.setBounds(250, 90, 100, 35);
+        label3.setBounds(400, 90, 100, 35);
+
+        this.frame.add(label1);
+        this.frame.add(label2);
+        this.frame.add(label3);
 
     }
 
@@ -66,20 +75,16 @@ public class UrnaVirtualView extends View {
 
     @Override
     public void notifyChage() {
-        // TODO Auto-generated method stub
-        System.out.println("Change notification from model into view: com.beluga.impl.urnaVirtual");
-        System.out.println(((UrnaVirtual) this.model).getCandidatos().get(0).getNombre());
-        System.out.println(((UrnaVirtual) this.model).getCandidatos().get(0).getVotos());
-        System.out.println(((UrnaVirtual) this.model).getCandidatos().get(1).getNombre());
-        System.out.println(((UrnaVirtual) this.model).getCandidatos().get(1).getVotos());
-        System.out.println(((UrnaVirtual) this.model).getCandidatos().get(2).getNombre());
-        System.out.println(((UrnaVirtual) this.model).getCandidatos().get(2).getVotos());
-        label.setText(String.valueOf(++contador));
+
+        UrnaVirtual uv = (UrnaVirtual) this.model;
+        label1.setText(String.valueOf(uv.getCandidatos().get(0).getVotos()));
+        label2.setText(String.valueOf(uv.getCandidatos().get(1).getVotos()));
+        label3.setText(String.valueOf(uv.getCandidatos().get(2).getVotos()));
 
     }
 
     @Override
     public void show() {
-        this.frame.setVisible(true);        
+        this.frame.setVisible(true);
     }
 }
